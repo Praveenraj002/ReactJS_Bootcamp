56ea0c0da19a5552b18044e36043b4f1a86d7550
Praveen Raj <77684640+Praveenraj002@users.noreply.github.com>
2021-06-26 15:19:47 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+
+var curryear = new Date().getFullYear
+    return(
+        <footer>
+            <p>copyright @ {curryear}</p>
+        </footer>
+        
+    );
+}
+
+export default Footer;
+
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+    return(
+        <div className="note">
+            <h1>Java Script and React. js</h1>
+            <p>this was an Amazing Bootcamp Taken up by Shaurya Sinha. We Covered eberything from Scratch including Javascript and React.js, HTML.</p>
+        </div>
+        
+    );
+}
+
+export default Note;
+
